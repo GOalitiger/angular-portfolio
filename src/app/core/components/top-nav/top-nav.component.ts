75b948f457277488b0967fc757ff72933201608f
AliMuazzam <alimuazzam93@gmail.com>
2023-06-15 00:48:22 +0500

    feat:Added basic layout and setting the basic layout properl

--- src/app/core/components/top-nav/top-nav.component.ts
+++ src/app/core/components/top-nav/top-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,4 +9,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   styleUrls: ['./top-nav.component.scss'],
   // standalone: true,
 })
-export class TopNavComponent {}
+export class TopNavComponent {
+  @Output() sideNav = new EventEmitter<Boolean>();
+  outputToOpenSideNav() {
+    this.sideNav.emit(true);
+  }
+}
